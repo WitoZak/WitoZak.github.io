702ba2f43e053d9bb043aeb3572a20311076193a
WitoZak <113088417+WitoZak@users.noreply.github.com>
2023-03-16 11:08:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://task.wito16.repl.co/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello';
+  const apiRoot = 'https://replit.com/@wito16/task';
+  const trelloApiRoot = 'https://replit.com/@wito16/task/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
